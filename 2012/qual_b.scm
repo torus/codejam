fe872ec1d78f2204d8c5256ab9d3473abeb84b9c
Toru Hisai <toru@torus.jp>
2012-04-16 02:55:05 +0900

    indent

--- 2012/qual_b.scm
+++ 2012/qual_b.scm
@@ -27,38 +27,38 @@
 
 (define (num-high-scorer total-list len num-surprise p)
   (define (iter total-list len num-surprise partial)
-  ;; total-list must be sorted high to low
-  (if (null? total-list)
-      (if (= num-surprise 0) partial #f)
-      (if (or (> num-surprise len)
-              (< num-surprise 0))
-          #f
-          (let ((total (car total-list)))
-            (let-values (((min-score min-score-surp) (guess-min-score total))
-                         ((max-score max-score-surp) (guess-max-score total)))
-              (if (and (or (= (remainder total 3) 1)
-                           (>= max-score p))
-                       (< num-surprise len))
-                  #;(iter (cdr total-list) (- len 1) num-surprise
-                                   (+ (if (>= max-score p) 1 0) partial))
-                  (choose-better
-                   (iter (cdr total-list) (- len 1) num-surprise
-                                    (+ (if (>= max-score p) 1 0) partial))
-                   (and
-                    (not (or (< min-score-surp 0) (> max-score-surp 10)))
-                    (> num-surprise 0)
-                    (iter (cdr total-list) (- len 1) (- num-surprise 1)
-                                     (+ (if (>= max-score-surp p) 1 0) partial))))
-                  (or
-                   (and
-                    (not (or (< min-score-surp 0) (> max-score-surp 10)))
-                    (> num-surprise 0)
-                    (iter (cdr total-list) (- len 1) (- num-surprise 1)
-                                     (+ (if (>= max-score-surp p) 1 0) partial)))
-                   (iter (cdr total-list) (- len 1) num-surprise
-                                    (+ (if (>= max-score p) 1 0) partial)))
-                  )
-              )))))
+    ;; total-list must be sorted high to low
+    (if (null? total-list)
+        (if (= num-surprise 0) partial #f)
+        (if (or (> num-surprise len)
+                (< num-surprise 0))
+            #f
+            (let ((total (car total-list)))
+              (let-values (((min-score min-score-surp) (guess-min-score total))
+                           ((max-score max-score-surp) (guess-max-score total)))
+                (if (and (or (= (remainder total 3) 1)
+                             (>= max-score p))
+                         (< num-surprise len))
+                    #;(iter (cdr total-list) (- len 1) num-surprise
+                    (+ (if (>= max-score p) 1 0) partial))
+                    (choose-better
+                     (iter (cdr total-list) (- len 1) num-surprise
+                           (+ (if (>= max-score p) 1 0) partial))
+                     (and
+                      (not (or (< min-score-surp 0) (> max-score-surp 10)))
+                      (> num-surprise 0)
+                      (iter (cdr total-list) (- len 1) (- num-surprise 1)
+                            (+ (if (>= max-score-surp p) 1 0) partial))))
+                    (or
+                     (and
+                      (not (or (< min-score-surp 0) (> max-score-surp 10)))
+                      (> num-surprise 0)
+                      (iter (cdr total-list) (- len 1) (- num-surprise 1)
+                            (+ (if (>= max-score-surp p) 1 0) partial)))
+                     (iter (cdr total-list) (- len 1) num-surprise
+                           (+ (if (>= max-score p) 1 0) partial)))
+                    )
+                )))))
   (iter total-list len num-surprise 0))
 
 (define (main args)
